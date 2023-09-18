c0c81e0a95f01d54122395638693185a3b5ef63d
hessdale <hessdale96@gmail.com>
2023-09-18 15:54:47 -0700

    working with discogs api and trying to get authorization and album images to work

--- src/components/GetAlbums.vue
+++ src/components/GetAlbums.vue
@@ -5,8 +5,17 @@
 </template>
 
 <script>
+import axios from "axios";
     export default {
-        
+        mounted () {
+            axios.request({
+                url:`https://api.discogs.com/users/north420/collection/folders/0/releases`,
+            }).then((response)=>{
+                console.log(response)
+            }).catch((error)=>{
+                console.log(error)
+            });
+        },
     }
 </script>
 
