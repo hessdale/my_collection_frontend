46a000bcdae2626f761d61604d81a3d92a6519df
hessdale <hessdale96@gmail.com>
2023-09-18 19:17:03 -0700

    figured out images showing up from collection through discogs api

--- src/components/GetAlbums.vue
+++ src/components/GetAlbums.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-
+<article v-for="(album,i) in albums" :key="i">
+    <img :src="get_imageURL(i)" width="200px">
+</article>
     </div>
 </template>
 
 <script>
 import axios from "axios";
     export default {
+        methods: {
+            get_imageURL(i) {
+                let image_URL = this.albums[i].basic_information.cover_image
+                return image_URL
+            }
+        },
+        data() {
+            return {
+                albums:undefined
+            }
+        },
         mounted () {
             
             axios.request({
@@ -15,7 +28,8 @@ import axios from "axios";
                     'Authorization':`Discogs token=lnUqECsqzdHkSZAGdDCBjdeQTPgbKxkhMwMMWbDr`,
                 }
             }).then((response)=>{
-                console.log(response)
+                this.albums=response.data.releases
+                console.log(this.albums[0].basic_information.cover_image)
             }).catch((error)=>{
                 console.log(error)
             });
