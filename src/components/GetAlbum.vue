892937f14053117f57746c7c24ea445fce451e30
hessdale <hessdale96@gmail.com>
2024-01-23 17:03:28 -0800

    moved album view code into seperate component called GetAlbum

---
+++ src/components/GetAlbum.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <img :src="album_info.images[0].resource_url" alt="album cover" width="400px">
+        <div>
+            <h3>{{ album_info.artists_sort }} - {{ album_info.title }}</h3>
+            <p>{{ album_info.notes }}</p>
+        </div>
+        <div v-for="(video, i) in album_info.videos" :key="i">
+            <a :href="video.uri">{{ video.title }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            album_info: undefined
+        }
+    },
+    mounted() {
+        axios.request({
+            url: "https://api.discogs.com/releases/" + this.$route.params.id,
+            headers: {
+                'Authorization': `Discogs token=lnUqECsqzdHkSZAGdDCBjdeQTPgbKxkhMwMMWbDr`,
+            }
+        }).then((response) => {
+            console.log(response)
+            this.album_info = response.data;
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+}
+</script>
+
+<style scoped></style>
--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -1,36 +1,14 @@
 <template>
     <div>
-        <img :src="album_info.images[0].resource_url" alt="album cover" width="400px">
-        <div>
-            <h3>{{ album_info.artists_sort }} - {{ album_info.title }}</h3>
-            <p>{{ album_info.notes }}</p>
-        </div>
-        <div v-for="(video, i) in album_info.videos" :key="i">
-            <a :href="video.uri">{{ video.title }}</a>
-        </div>
+        <get-album></get-album>
     </div>
 </template>
 
 <script>
-import axios from "axios";
+import GetAlbum from '@/components/GetAlbum.vue';
 export default {
-    data() {
-        return {
-            album_info: undefined
-        }
-    },
-    mounted() {
-        axios.request({
-            url: "https://api.discogs.com/releases/" + this.$route.params.id,
-            headers: {
-                'Authorization': `Discogs token=lnUqECsqzdHkSZAGdDCBjdeQTPgbKxkhMwMMWbDr`,
-            }
-        }).then((response) => {
-            console.log(response)
-            this.album_info = response.data;
-        }).catch((error) => {
-            console.log(error);
-        });
+    components: {
+        GetAlbum,
     },
 }
 </script>
